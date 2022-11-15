f761a64feb084305bf408db47ca03d6213b82b5b
CSW0126 <shuiwing123@gmail.com>
2022-11-15 15:23:40 +0800

    Update daymeal.js

--- Server/models/daymeal.js
+++ Server/models/daymeal.js
@@ -3,5 +3,7 @@ const Food = require('./food')
 
 module.exports = new mongoose.Schema({
     date : {type:Date , require: true},
+    //breakfast, lunch, dinner, other
+    type: {type: String, default: ""},
     food: {type:[Food], default:[]}
 })
