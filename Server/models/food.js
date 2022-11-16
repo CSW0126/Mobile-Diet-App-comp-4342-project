abb0fe1739b0e0a7556f30db4fb1d30cf9426313
kwongyk123 <67676010+kwongyk123@users.noreply.github.com>
2022-11-16 20:13:22 +0800

    update

--- Server/models/food.js
+++ Server/models/food.js
@@ -7,41 +7,11 @@ module.exports = new mongoose.Schema({
     imageUrl : {type:String, default: ""},
     unit : {type: String, default: ""},
     quantity :{type: Number, default: 1},
-    nutrition: {type: Nutrition, default: ()=>{return new NutritionModel({})}}
-
-    
+    nutrition: {type: Nutrition, default: ()=>{return new NutritionModel({})}},
 })
 
 
-function foodRec(){
-    const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
-    const stub = ClarifaiStub.grpc();
-    const metadata = new grpc.Metadata();
-    metadata.set("authorization", "Key " +  process.env.FOOD_REC_API_KEY);
-    const imageBytes = fs.readFileSync("uploads/apple.jpg"); 
 
-    stub.PostModelOutputs(
-        {
-            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-            model_id: process.env.FOOD_REC_MODEL_ID,
-            inputs: [{data: {image: {base64 : imageBytes}}}]
-        },
-        metadata,
-        (err, response) => {
-            if (err) {
-                console.log("Error: " + err);
-                return;
-            }
 
-            if (response.status.code !== 10000) {
-                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-                return;
-            }
 
-            console.log("Predicted concepts, with confidence values:")
-            for (const c of response.outputs[0].data.concepts) {
-                console.log(c.name + ": " + c.value);
-            }
-        }
-    );
-}
+
--- Server/routes/foodRec.js
+++ Server/routes/foodRec.js
@@ -8,6 +8,7 @@ const multer = require('multer');
 const fs = require("fs");
 var mime = require('mime-types')
 const { json } = require('body-parser');
+const { foodObject } = require('../models/food.js');
 
 router.post('/', (req, res, next) => {
     console.log(req.headers);
@@ -47,22 +48,85 @@ const upload = multer({
     fileFilter: fileFilter
 });
 
-router.post('/',upload.single('FoodImage'), (req, res, next) =>{
-    console.log(req.file);
-    // console.log(req.file.path);
-    const imageUrl = req.file.path;
+router.post('/',upload.single('FoodImage'), async (req, res, next) =>{
+    try{
+        console.log(req.file);
+        // console.log(req.file.path);
+        const imageUrl = req.file.path;
 
-    if (fs.existsSync(imageUrl)) {
-        const imageBytes = fs.readFileSync(imageUrl);
-        //call food Rec API
-        res.status(200).json({message:"finish"})
+        if (fs.existsSync(imageUrl)) {
+            const imageBytes = fs.readFileSync(imageUrl);
+            //call food Rec API
+            res.status(200).json({message:"finish"})
+         
+            const clarifaiRsult  =  await foodRec(imageBytes)
+            console.log("clarifaiRsult is ", clarifaiRsult)
+            
+            if(clarifaiRsult == null){
+                throw 'clarfai API error'
+            }
 
-    } else {
-        res.status(500).json({
-            message: 'upload fail'
-        })
+            const turnResult = await jsonToArrayToQuery(clarifaiRsult.turnResult)
+            console.log("what is this", turnResult)
+
+            const respFromNutriDB = await doNutritionixDBRequest(turnResult)
+
+            console.log(respFromNutriDB)
+
+        } else {
+            res.status(500).json({
+                message: 'upload fail'
+            })
+        }
+    }catch(err){
+
     }
 })
 
+
+
+
+function foodRec(imageBytes ){
+    const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+    const stub = ClarifaiStub.grpc();
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", "Key " +  process.env.FOOD_REC_API_KEY);
+    var jsonArr = [];
+
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: process.env.FOOD_REC_MODEL_ID,
+            inputs: [{data: {image: {base64 : imageBytes}}}]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                jsonArr.push({
+                    name: c.name,
+                    value: c.value
+                })
+            }
+            console.log("here we go ", jsonArr)
+            
+            if (typeof callback === 'function') {
+                return callback(jsonArr);
+              }
+        }
+        
+    );
+
+}
 //export
 module.exports = router;
