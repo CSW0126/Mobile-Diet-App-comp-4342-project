7e412cdf35259302ea6e9e55a6f73af0a7787fe2
CSW0126 <shuiwing123@gmail.com>
2022-11-21 22:31:26 +0800

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,21 @@ const app = require('./app')
 const port = process.env.PORT || 3000;
 const server = http.createServer(app)
 require('dotenv').config();
+var os = require('os');
 
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
 
 server.listen(port)
 //log
-console.log("Listen: " + process.env.HOST + ":" + port);
+for (let i in addresses){
+    console.log("Listen: [" +i+"] http://" + addresses[i] + ":" + port);
+}
